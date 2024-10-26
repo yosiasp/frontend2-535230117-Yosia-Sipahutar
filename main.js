0e217517f6141d53a00d929fb8358b7cb2d0e51b
Yosia Sipahutar <yosia.535230117@.stu.untar.ac.id>
2024-10-26 12:08:00 +0700

    pembeneraan terakhir

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
-    $('#subscribe').click(function() {
+    $('#subscribe-index').click(function() {
+        var name = $('#name').val().trim();
+        var email = $('#email').val().trim();
+        var errorMessage = $('#error-message');
+
+        if (name === "" || email === "") {
+            errorMessage.text("Please fill in all fields.").show();
+        } else {
+            errorMessage.hide();
+            $('#subscribeModal').modal('show');
+        }
+    });
+
+    $('#subscribe-cont').click(function() {
+        var name = $('#name').val().trim();
+        var email = $('#email').val().trim();
+        var errorMessage = $('#error-message');
+
+        if (name === "" || email === "") {
+            errorMessage.text("Please fill in all fields.").show();
+        } else {
+            errorMessage.hide();
+            $('#subscribeModal').modal('show');
+        }
+    });
+
+    $('#subscribe-about').click(function() { // Updated selector for About page
         var name = $('#name').val().trim();
         var email = $('#email').val().trim();
         var errorMessage = $('#error-message');
