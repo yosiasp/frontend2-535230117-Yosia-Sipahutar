3a1f6c655f5f2fb48cb8814b7345e2c0df5af104
Yosia Sipahutar <yosia.535230117@.stu.untar.ac.id>
2024-10-26 12:29:43 +0700

    melakukan perpihan code

--- main.js
+++ main.js
@@ -1,42 +1,22 @@
 $(document).ready(function() {
-    $('#subscribe-index').click(function() {
-        var name = $('#name').val().trim();
-        var email = $('#email').val().trim();
-        var errorMessage = $('#error-message');
+    function handleSubscriptionClick(selector) {
+        $(selector).click(function() {
+            var name = $('#name').val().trim();
+            var email = $('#email').val().trim();
+            var errorMessage = $('#error-message');
 
-        if (name === "" || email === "") {
-            errorMessage.text("Please fill in all fields.").show();
-        } else {
-            errorMessage.hide();
-            $('#subscribeModal').modal('show');
-        }
-    });
+            if (name === "" || email === "") {
+                errorMessage.text("Please fill in all fields.").show();
+            } else {
+                errorMessage.hide();
+                $('#subscribeModal').modal('show');
+            }
+        });
+    }
 
-    $('#subscribe-cont').click(function() {
-        var name = $('#name').val().trim();
-        var email = $('#email').val().trim();
-        var errorMessage = $('#error-message');
-
-        if (name === "" || email === "") {
-            errorMessage.text("Please fill in all fields.").show();
-        } else {
-            errorMessage.hide();
-            $('#subscribeModal').modal('show');
-        }
-    });
-
-    $('#subscribe-about').click(function() { // Updated selector for About page
-        var name = $('#name').val().trim();
-        var email = $('#email').val().trim();
-        var errorMessage = $('#error-message');
-
-        if (name === "" || email === "") {
-            errorMessage.text("Please fill in all fields.").show();
-        } else {
-            errorMessage.hide();
-            $('#subscribeModal').modal('show');
-        }
-    });
+    handleSubscriptionClick('#subscribe-index');
+    handleSubscriptionClick('#subscribe-cont');
+    handleSubscriptionClick('#subscribe-about');
 
     $('#contact-kontak').submit(function(event) {
         event.preventDefault(); // Prevent the default form submission
